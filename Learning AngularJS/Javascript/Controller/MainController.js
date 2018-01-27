f1ff3b30c8414e9d5d332aec687a3bdf0e1617be
lequynhhuong <34002480+lequynhhuong@users.noreply.github.com>
2018-01-27 19:42:46 +0900

    add in function to express likes and dislikes to post

--- Learning AngularJS/Javascript/Controller/MainController.js
+++ Learning AngularJS/Javascript/Controller/MainController.js
@@ -14,6 +14,7 @@ app.controller('MainController', [
             pubdate: new Date('2014', '03', '08'),
             cover: 'img/the-book-of-trees.jpg',
             likes: 0,
+            dislikes: 0,
           },
           {
             name: 'Program or be Programmed',
@@ -21,9 +22,13 @@ app.controller('MainController', [
             pubdate: new Date('2013', '08', '01'),
             cover: 'img/program-or-be-programmed.jpg',
             likes: 0,
+            dislikes: 0,
           },
         ];
     $scope.plusOne = function(index) {
       $scope.products[index].likes += 1;
+    };
+    $scope.minusOne = function(index) {
+      $scope.products[index].dislikes -= 1;
     };
   }]);
